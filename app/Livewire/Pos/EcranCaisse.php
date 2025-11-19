144b891734f223b46a5433370c50e1140c839bfe
Josias DJIOLGOU <josiasdjiolgou@gmail.com>
2025-11-19 14:17:33 +0100

    gestion de disfocntionnement dans la page principale de vente

--- app/Livewire/Pos/EcranCaisse.php
+++ app/Livewire/Pos/EcranCaisse.php
@@ -23,7 +23,8 @@ class EcranCaisse extends Component
     // Mode de paiement
     public string $modePaiement = 'espece';
     // Paiement en espèces : somme encaissée par le client
-    public float $sommeEncaissee = 0;
+    // Note: remove typed property to avoid Livewire compatibility issues with some versions
+    public $sommeEncaissee = 0;
     
     // UI States
     public bool $showConfirmation = false;
